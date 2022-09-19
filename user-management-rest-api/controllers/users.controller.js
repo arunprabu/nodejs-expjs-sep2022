81e79b9f638e3a0d5d98e2a3cba435bc08fef57c
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-09-19 16:28:28 +0530

    updated

--- user-management-rest-api/controllers/users.controller.js
+++ user-management-rest-api/controllers/users.controller.js
@@ -1,12 +1,17 @@
 // Create a new User 
 exports.createUser = (req, res) => {
-  console.log(req);
+  console.log(req.body);
 
+  /* 
+  1. get the req.body 
+  2. connect with db
+  3. insert the above data into db 
+  4. get the confirmation from db and send it back 
+  */
   const userData = {
     id: 999,
     ...req.body
   }
-
   res.json(userData);
 }
 
