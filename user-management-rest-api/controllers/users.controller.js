4da2fb817630940973f2917606698924a7ca037f
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-09-21 15:13:06 +0530

    updated

--- user-management-rest-api/controllers/users.controller.js
+++ user-management-rest-api/controllers/users.controller.js
@@ -1,18 +1,27 @@
+const User = require('../models/users.model');
+
 // Create a new User 
 exports.createUser = (req, res) => {
   console.log(req.body);
-
   /* 
-  1. get the req.body 
-  2. connect with db
+  1. get the req.body  [DONE]
+  2. connect with db [DONE]
   3. insert the above data into db 
   4. get the confirmation from db and send it back 
   */
-  const userData = {
-    id: 999,
-    ...req.body
-  }
-  res.json(userData);
+
+  // creating DAO 
+  const userDao = new User(req.body);
+  // saving to create a new document by folowing syntax of mongoose
+  userDao.save( (err, data) => {
+    if(!err){
+      console.log(data);
+      res.json(data);
+    }else{
+      console.log(err);
+      res.json(err);
+    }
+  });  
 }
 
 // Get Users  (listing users )
